b4ab2c492c98eacd577ce9d77b5beacb3176af81
moni <vmoni1201@gmail.com>
2026-06-27 14:54:47 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -5,30 +5,27 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-// Middleware
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://YOUR-ACTUAL-VERCEL-APP.vercel.app"
-    ]
+      "https://personal-task-manager-mauve.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
   })
 );
 
 app.use(express.json());
 
-// Routes
 app.use("/api/tasks", taskRoutes);
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("Personal Task Manager API Running");
 });
 
-// Port
 const PORT = process.env.PORT || 5000;
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
