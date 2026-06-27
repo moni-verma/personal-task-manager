fbbe14247764f1708841c56e8ba8608373d5f4c2
moni <vmoni1201@gmail.com>
2026-06-27 14:14:26 +0530

    Fix CORS

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://personal-task-manager-l1ng.onrender.com/api/tasks";
 
 
 export const getTasks = async () => {
